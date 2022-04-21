230cf1bcafd0b9e98352dbfc55e1e68b6283801a
Odencrants Martin ML/MA/MET-Ö <martin.odencrants@scb.se>
2022-04-21 09:18:15 +0200

    path change

--- R/getMyData.R
+++ R/getMyData.R
@@ -1,16 +1,16 @@
 getMyData <- function(produkt,HP,refPeriod){
   if (HP==1) {
     if (refPeriod==2015){
-      omsdata<-read.csv(file ="H:/Granskning/Testdata.csv")
+      omsdata<-read.csv(file ="work/Testdata.csv")
     } else if (refPeriod==2016){
-      omsdata<-read.csv(file ="H:/Granskning/omsData2016Q1TF.csv")
+      omsdata<-read.csv(file ="work/omsData2016Q1TF.csv")
     }
     return(omsdata)
   } else if (HP==2){
     if (refPeriod==2015){
-      omsdata<-read.csv(file ="H:/Granskning/omsData2016Q1edit.csv")
+      omsdata<-read.csv(file ="work/omsData2016Q1edit.csv")
     } else if (refPeriod==2016){
-      omsdata<-read.csv(file ="H:/Granskning/omsData2016Q1edit.csv")
+      omsdata<-read.csv(file ="work/omsData2016Q1edit.csv")
     }
     return(omsdata)
   }
